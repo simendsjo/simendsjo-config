03d25f9dff153fa840db3f15b2f9f1a3a5537429
Simen Endsjø <simendsjo@gmail.com>
2015-11-23 20:52:24 +0100

    Forgot to add before pushing...

--- install-scripts/link-vim.sh
+++ install-scripts/link-vim.sh
@@ -1,4 +1,7 @@
+# Note that this script only aliases configurations.
+# It does not install neovim or vim itself.
 SRC=~/simendsjo-config/.config/nvim
 ln -s $SRC ~/.config/nvim
+# Use same config files for regular vim
 ln -s $SRC ~/.vim
 ln -s $SRC/init.vim ~/.vimrc
